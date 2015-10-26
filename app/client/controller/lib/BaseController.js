e94a13fab074677bb3534278939fb37f2a69611a
nizulzaim <shimizukazunari93@gmail.com>
2015-10-26 23:58:31 +0800

    Fix some foldering error

--- app/client/controller/lib/BaseController.js
+++
@@ -1,8 +0,0 @@
-BaseController = RouteController.extend({
-    layoutTemplate:"ApplicationLayout",
-    notFoundTemplate:"404",
-    yieldTemplates: {
-       'defaultToolbar': {to: 'toolbar'},
-       'defaultNavigation': {to: "navigation"}
-    }
-});
